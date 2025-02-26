dc5e673ae064463e441dd842f7f9501f179b54ca
julienlucas <contact@julienlucas.com>
2025-02-26 17:18:59 +0100

    First commit

---
+++ index.ts
@@ -0,0 +1,15 @@
+import { textGeneration } from "@huggingface/inference";
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const HF_ACCESS_TOKEN = process.env.HF_ACCESS_TOKEN;
+
+let result = await textGeneration({
+    accessToken: HF_ACCESS_TOKEN,
+    model: 'gpt2',
+    inputs: 'The answer to the universe is',
+});
+
+console.log(result);
+/* { generated_text: 'The answer to the universe is not to be found in the universe itself, but in the universe itself' } */
